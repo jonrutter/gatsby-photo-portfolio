5a02b5a19208d3d3cf6ad1b3d09b97124993f01f
Jon Rutter <contact@jonrutter.com>
2022-02-03 20:49:35 -0500

    Global styles

---
+++ src/components/Layout/globalStyles.js
@@ -0,0 +1,77 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    font-size: 100%;
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-size: 1rem;
+    line-height: 1.5;
+    font-family: Montserrat, sans-serif;
+    overflow-x: hidden;
+  }
+
+  button {
+    font-size: 100%;
+    font-family: inherit;
+    line-height: inherit;
+    cursor: pointer;
+    border: none;
+    margin: 0;
+    text-transform: none;
+    -webkit-appearance: none;
+  }
+
+  a {
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  a,
+  button {
+    background-color: transparent;
+  }
+
+  a,
+  a:hover {
+    color: inherit;
+    font-size: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: normal;
+    font-size: 1.5rem;
+    padding: 1rem;
+  }
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  img {
+    user-select: none;
+  }
+
+  address {
+    font-style: normal;
+  }
+`;
