914a4599d1ed6af542d9c9c5bee377a98c31805a
Jon Rutter <contact@jonrutter.com>
2022-02-04 19:28:37 -0500

    Page title component

---
+++ src/components/Title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  padding: 0;
+  margin-bottom: 1rem;
+  @media (min-width: 576px) {
+    font-size: 2rem;
+  }
+`;
+
+export default Title;
